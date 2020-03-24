69099f620519c9ff5d24a370f92531ea421df4ad
Higor Shindy Kudo <higorshun@icloud.com>
2020-03-25 05:07:05 +0900

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Higor Shindy Kudo on 2020/03/24.
+//  Copyright © 2020 Higor Shindy Kudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
